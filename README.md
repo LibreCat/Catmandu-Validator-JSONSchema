923358f581a5935b8b3f888ff82d38b6ecaf8f26
Nicolas Franck <nicolas.franck@ugent.be>
2014-05-11 19:40:09 +0200

    added README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+Catmandu-Validator-JSONSchema
+========
+
+## PURPOSE
+
+    -   Define an interface for validations in Catmandu. This interface should be included
+        in the main Catmandu module
+    -   Make an implementation for this module to validate regular perl objects against
+        a JSON Schema (see JSON::Schema on cpan)
+
+## AUTHOR
+
+Nicolas Franck
--- lib/Catmandu/Validator/JSONSchema.pm
+++ lib/Catmandu/Validator/JSONSchema.pm
@@ -9,50 +9,6 @@ our $VERSION = "0.1";
 
 with qw(Catmandu::Validator);
 
-=head1 PROPOSAL
-
-use Catmandu;
-use Catmandu::Env;
-
-sub Catmandu::Env::default_validator {
-    'default';
-}
-sub Catmandu::Env::validator_namespace { 
-    'Catmandu::Validator';
-}
-sub Catmandu::Env::validators {
-    state $validators = {};
-    $validators;
-}
-sub Catmandu::Env::validator {
-    my $self = shift;
-    my $name = shift;
-
-    my $validators = $self->validators;
-
-    my $key = $name || $self->default_validator;
-
-    $validators->{$key} || do {
-        my $ns = $self->validator_namespace;
-        if (my $c = $self->config->{validator}->{$key}) {
-            check_hash_ref($c);
-            check_string(my $package = $c->{package});
-            my $opts = $c->{options} || {};
-            if (@_ > 1) {
-                $opts = {%$opts, @_};
-            } elsif (@_ == 1) {
-                $opts = {%$opts, %{$_[0]}};
-            }
-            return $validators->{$key} = require_package($package, $ns)->new($opts);
-        }
-        if ($name) {
-            return require_package($name, $ns)->new(@_);
-        }
-        Catmandu::BadArg->throw("unknown validator ".$self->default_validator);
-    }
-}
-=cut
-
 has schema => (
     is => 'rw',
     isa => sub { check_hash_ref($_[0]); },
