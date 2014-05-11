f6db6c905817c24c2cb6009bef84995090a8eb08
Nicolas Franck <nicolas.franck@ugent.be>
2014-05-11 20:56:35 +0200

    Catmandu::Validation::Result

--- lib/Catmandu/Validation/Result.pm
+++ lib/Catmandu/Validation/Result.pm
@@ -2,17 +2,14 @@ package Catmandu::Validation::Result;
 use Catmandu::Sane;
 use Moo;
 
-has _errors => (
+has errors => (
     is => 'rw',
     isa => sub { check_array_ref($_[0]); },
     lazy => 1,
-    default => sub { []; },
-    handles => {
-        errors => 'elements',
-        has_errors => 'count',
-        clear_errors => 'clear'
-    }
+    default => sub { []; }
 );
-sub valid { !($_[0]->has_errors()); }
+sub valid { 
+    scalar(@{ $_[0]->errors() }) == 0; 
+}
 
 1;
